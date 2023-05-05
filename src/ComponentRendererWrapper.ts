65fe19ec60124b9a1194919c9cdc699fe3e316b4
krocon@gmail.com <xPQxsnNVLNcfbRFGngY2>
2023-05-05 16:15:25 +0200

    init

---
+++ src/ComponentRendererWrapper.ts
@@ -0,0 +1,40 @@
+import { AreaIdent, AreaModelIf, CellRendererIf, DomServiceIf, RendererCleanupFnType } from "@guiexpert/table";
+import { JSX } from "solid-js/jsx-runtime";
+import { mergeProps, render } from "solid-js/web";
+
+//import {RendererPropsIf}  from "./RendererPropsIf";
+
+
+export class ComponentRendererWrapper implements CellRendererIf {
+
+  constructor(
+    private readonly code: (props: any) => JSX.Element
+  ) {
+  }
+
+  render(
+    cellDiv: HTMLDivElement,
+    rowIndex: number,
+    columnIndex: number,
+    areaIdent: AreaIdent,
+    areaModel: AreaModelIf,
+    cellValue: any,
+    _domService: DomServiceIf): RendererCleanupFnType | undefined {
+
+    const props = mergeProps({
+      rowIndex,
+      columnIndex,
+      areaIdent,
+      areaModel,
+      cellValue
+    });
+    render(
+      () => this.code(props),
+      cellDiv
+    );
+
+    return undefined;
+  }
+
+
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import SolidTable from "./SolidTable";
+import { ComponentRendererWrapper } from "./ComponentRendererWrapper";
+
+export {
+  SolidTable,
+  ComponentRendererWrapper
+};
